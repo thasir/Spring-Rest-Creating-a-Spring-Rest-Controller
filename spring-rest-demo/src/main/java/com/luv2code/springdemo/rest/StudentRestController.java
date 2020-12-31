26d77c58591dab21c7fa34ba75e1f2673a56a86a
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-31 18:22:50 +0530

    Spring REST - Global Exception Handling with @ControllerAdvice

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -43,28 +43,4 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 
-	// add exception handler using @ExceptionHandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-		// create a student error response
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		// return response entity
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
-
-	// add exception handler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-		// create a student error response
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		// return response entity
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-
-	}
 }
