50ba5ceae8dbaa3d99ebc5a1f13dc7ee3767ee27
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-31 15:55:23 +0530

    using @PathVariables for REST Endpoints

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +15,25 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 		theStudents.add(new Student("Thasir", "Ahmed"));
 		theStudents.add(new Student("Jimothy", "Timothy"));
 		theStudents.add(new Student("Jim", "Bob"));
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 
 		return theStudents;
+	}
 
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		return theStudents.get(studentId);
 	}
 }
